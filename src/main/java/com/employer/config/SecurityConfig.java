0eac0de7d6a66431f6ae6c5ea18cddbebe555bee
Siddaram <eparis@atlassian.com>
2019-12-08 14:17:57 +0530

    basic auth changes

--- src/main/java/com/employer/config/SecurityConfig.java
+++ src/main/java/com/employer/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.employer.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,9 +17,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
 	}
+	
+	@Value("${application.fid}")
+	private String appFid;
+	
+	@Value("${application.fpwd}")
+	private String appFpwd;
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}password").roles("USER");
+		auth.inMemoryAuthentication().withUser(appFid).password(passwordEncoder().encode(appFpwd)).roles("USER");
 	}
+	
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
